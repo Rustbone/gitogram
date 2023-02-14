fdd41d59edb02d9edefa295e17675c71fe044d1c
Rustbone <rustem.abd@mail.ru>
2023-02-14 23:09:43 +0300

    1.2.1 week

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
 // transpileDependencies: true
-  publicPath: process.env.NODE_ENV === 'production' ? '/gitogram/dist/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/gitogram/' : '/'
 }
